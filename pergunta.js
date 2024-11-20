11fd95bbfad66792dd74b4977f5cb64fcce803b3
crisleosantos <00001094820623sp@al.educacao.sp.gov.br>
2024-11-19 21:13:32 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+<script src="app.js"></script>
+<script src="perguntas.js"></script>
+
+criaCartao(categoria, pergunta, resposta) {}
+
+function criaCartao(categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
+
+function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container')
+}
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
+
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+}
+
